479a7f71a3b6ddfdf9ad98df1e071932e40c1f8f
Администратор <abdusattar.kasymbekov@gmail.com>
2022-05-06 15:18:07 +0600

    Added entites

---
+++ CompanyManagerMVC/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyManagerMVC.Entities
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Posts = new HashSet<Post>();
+            Tasks = new HashSet<Task>();
+            Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ CompanyManagerMVC/Entities/TaskStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyManagerMVC.Entities
+{
+    public partial class TaskStatus
+    {
+        public TaskStatus()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
